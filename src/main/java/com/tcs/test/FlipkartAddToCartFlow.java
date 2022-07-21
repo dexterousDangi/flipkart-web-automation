c549985cf701555254b356e2aff758ccd272d605
desterousDangi <inboxpardeepdangi@gmail.com>
2022-07-21 21:09:13 +0530

    first script to login into the Flipkart account

---
+++ src/main/java/com/tcs/test/FlipkartAddToCartFlow.java
@@ -0,0 +1,56 @@
+package com.tcs.test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import com.tcs.driver.Driver;
+
+public class FlipkartAddToCartFlow {
+	public static WebDriver driver=null;
+	
+	public static void main(String[] args) throws InterruptedException {
+		 
+		/**
+		 * Get the driver and navigate to Flipkart
+		 * this approach will help us to use multiple drivers
+		 */
+		driver =Driver.getChromeDriver();	
+		driver.manage().window().maximize();
+     	driver.navigate().to("https://www.flipkart.com");
+     	
+     	/**
+     	 *This method is used to login on the website
+     	 */
+		login("9015677021","Flipkart@123");
+		
+		/**
+		 * This method is for loging out form the site
+		 */
+		logout();
+		
+	}
+
+
+
+	private static void login(String username,String password) throws InterruptedException {
+		
+		Thread.sleep(1000);
+		
+		WebElement loginBox=driver.findElement(By.cssSelector("div._36HLxm.col.col-3-5 > div > form > div:nth-child(1) > input"));
+		loginBox.sendKeys(username);
+		
+		Thread.sleep(1000);
+
+		WebElement passwordBox=driver.findElement(By.cssSelector("input[type=password]"));
+		passwordBox.sendKeys(password);	
+		
+		WebElement submitButton=driver.findElement(By.cssSelector("button[type=submit] span"));
+		submitButton.submit();		
+		
+	}
+
+}
